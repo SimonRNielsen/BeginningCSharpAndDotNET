5a8a16e7ce04424d881966fbcf74e3d9b803f03e
SimonRNielsen <simonrn@gmail.com>
2024-09-23 16:21:58 +0200

    1-3

---
+++ Chapter02/ConsoleApp1_Simon/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The First app in Beginning C# and .NET!");
+            Console.ReadKey();
+        }
+    }
+}
